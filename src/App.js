41939df0952b40973399000c6461a9a60040ac48
Dawid <daw.tomczyk1997@gmail.com>
2023-04-21 17:28:07 +0200

    react router implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
-import './App.css';
+import Homepage from './pages/Homepage/Homepage';
+import Country from './pages/Country/Country';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="/country/:name" element={<Country/>} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
