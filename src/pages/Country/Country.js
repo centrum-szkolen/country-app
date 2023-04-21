3c3a6c0c0fa3fa21ed7915f728958becb382f540
Dawid <daw.tomczyk1997@gmail.com>
2023-04-21 19:49:04 +0200

    fetch country by name

--- src/pages/Country/Country.js
+++ src/pages/Country/Country.js
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 
 const Country = () => {
   const [current, setCurrent] = useState();
+  const {name} = useParams();
+  console.log(name);
 
   useEffect(()=>{
      getCountryByName();
   },[])
 
   const getCountryByName = async () => {
-    const response = await fetch(`https://restcountries.com/v3.1/name/poland`);
+    const response = await fetch(`https://restcountries.com/v3.1/name/${name}`);
     const data = await response.json();
     setCurrent(data[0]);
   };
 
   console.log(current)
+
+  if(!current) return "Loading...."
 
   return (
     <div>
